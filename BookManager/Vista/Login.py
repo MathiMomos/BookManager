4def4e2dae089d0ab713c8d6df134c570fcbb50d
Gerardo <gerarleonce-0703@outlook.es>
2024-11-05 20:56:19 -0500

    Se agregó la funcionalidad de validar credenciales

--- BookManager/Vista/Login.py
+++ BookManager/Vista/Login.py
@@ -2,45 +2,67 @@ from tkinter import Tk, StringVar
 from tkinter import ttk
 from BookManager.BookManager.Vista.Vendedor.Vender import Vender
 
+import sqlite3
+import hashlib
+
 class Login(Tk):
     def __init__(self):
         super().__init__()
         self.title("Login")
         self.geometry("400x300")
 
-        self.role_var = StringVar(value="Vendedor")
+        self.var_rol = StringVar(value="Vendedor")
 
         ttk.Label(self, text="Usuario:").pack(pady=5)
-        self.username_entry = ttk.Entry(self)
-        self.username_entry.pack(pady=5)
+        self.entrada_usuario = ttk.Entry(self)
+        self.entrada_usuario.pack(pady=5)
 
         ttk.Label(self, text="Contraseña:").pack(pady=5)
-        self.password_entry = ttk.Entry(self, show="*")
-        self.password_entry.pack(pady=5)
+        self.entrada_contrasenia = ttk.Entry(self, show="*")
+        self.entrada_contrasenia.pack(pady=5)
 
         ttk.Label(self, text="Rol:").pack(pady=5)
-        ttk.Radiobutton(self, text="Administrador", variable=self.role_var, value="Administrador").pack()
-        ttk.Radiobutton(self, text="Vendedor", variable=self.role_var, value="Vendedor").pack()
+        ttk.Radiobutton(self, text="Administrador", variable=self.var_rol, value="admin").pack()
+        ttk.Radiobutton(self, text="Vendedor", variable=self.var_rol, value="usuario").pack()
 
         ttk.Button(self, text="Login", command=self.login).pack(pady=20)
 
     def login(self):
-        username = self.username_entry.get()
-        password = self.password_entry.get()
-        role = self.role_var.get()
+        username = self.entrada_usuario.get()
+        password = self.entrada_contrasenia.get()
+        role = self.var_rol.get()
 
-        if self.validate_credentials(username, password, role):
+        if self.validar_credenciales(username, password, role):
             self.withdraw()  # Oculta la ventana de Login en lugar de destruirla
-            if role == "Administrador":
+            if role == "admin":
                 print("Administrador")
+            elif role == "usuario":
+                self.abrir_interfaz_vendedor()
             else:
-                self.open_vendedor_interface()
+                print("Rol no válido")
 
-    def validate_credentials(self, username, password, role):
-        # Aquí puedes agregar la lógica para validar las credenciales
-        return True
+    def validar_credenciales(self, username, password, role):
+        # Conexión a la base de datos
+        conexion = sqlite3.connect(r'C:\Users\USER\PortafolioUNMSM\Estructura de datos\Proyecto\BookManager\BookManager\Data\users.db')
+        cursor = conexion.cursor() #Para poder ejecutar los comandos
 
-    def open_vendedor_interface(self):
+        # Consulta a la base de datos
+        cursor.execute("SELECT * FROM users WHERE username = ? AND role = ?", (username, role))
+        resultado = cursor.fetchone()
+
+        # Cerrar la conexión
+        cursor.close()
+        conexion.close()
+
+        if resultado :
+            contrasenia_almacenada = resultado[2]
+            # Comparamos la contraseña ingresada con la almacenada
+            contrasenia_hash = hashlib.sha256(password.encode()).hexdigest()
+            if contrasenia_hash == contrasenia_almacenada:
+                return True
+        return False
+
+    def abrir_interfaz_vendedor(self):
         app = Vender()
         app.mainloop()
 
