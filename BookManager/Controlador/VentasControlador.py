a56ba3552a9f0228d4a9474ae01cf9f6a84384cc
MagisterAhriman15 <germina3915@gmail.com>
2024-11-06 19:15:02 -0500

    Eliminar codigo repetido

--- BookManager/Controlador/VentasControlador.py
+++ BookManager/Controlador/VentasControlador.py
@@ -8,17 +8,6 @@ class VentasControlador:
 
     #def __init__(self):
     #una vez ya conectado con la BD para Producto
-    def registrarVenta(self):
-        if producto.empty:
-            print(f"Producto {nombre} no encontrado en el inventario")
-            return
-
-        print(f"Venta registrada: {cantidad} unidades de '{nombre}' por un total de {total} soles.")
-    
-    def verHistorialDeVentas(self):
-        #BD para historial de ventas
-        if historial.empty:
-            print(f"Historial de ventas para '{vendedor}': {historial}")
 
     def generarTicket(self):
         print (f"Generando ticket en PDF para la venta ID {ventaID}")
